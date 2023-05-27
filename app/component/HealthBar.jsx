7c0fcb7a9ace92f43e0f38db171652f5e64db34c
raihanmakarim21 <raihanmakarim21@gmail.com>
2023-05-28 06:01:02 +0700

    add more features, add artwork

--- app/component/HealthBar.jsx
+++ app/component/HealthBar.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const healthBar = ({ color,health }) => (
   <div id="health-bar" className="rounded-full" style={{
-    width: `40vw`,height: `5vh `, background: "grey", 
+    width: `40vw`,height: `3vh `,maxHeight: "250px", background: "grey", 
   }}>
     <div
       style={{
         width: `${health}%`,height: `100%`, backgroundColor: color 
       }}
-      className={`shadow-none flex   rounded-full `}
+      className={`shadow-none flex   rounded-full transition-all duration-400 ease-in-out `}
     />
   </div>
 );
--- app/page.js
+++ app/page.js
@@ -3,6 +3,17 @@ import React, {
   useState, useEffect, useRef 
 } from "react";
 import HealthBar from "./component/HealthBar";
+
+import idle from "../public/idle.png";
+import prepare_jab from "../public/prepare_jab.png";
+
+import jab from "../public/jab.png";
+
+import Image from 'next/image';
+import Opponent_iddle from "../public/Opponent_iddle.png";
+import Opponent_jab from "../public/Opponent_jab.png";
+import Opponent_prepare_jab from "../public/Opponent_prepare_jab.png";
+
 const RANDOM_QUOTE_API_URL = "https://api.quotable.io/random?minLength=200&maxLength=240";
 
 
@@ -25,7 +36,7 @@ const Word = React.memo(function Word(props) {
     className += " text-black";
   }
 
-  return <span className={`quote-display ${className}`}>{word} </span>;
+  return <div className={`quote-display ${className}`}>{word} </div>;
 });
 
 
@@ -44,7 +55,51 @@ const Word = React.memo(function Word(props) {
 
 
 
+const Boxer = ({
+  isTransitioning, images, stopIndex = 2 , right,left,bottom, zIndex,width =600,height =600,className
+}) => {
+  const [ currentImageIndex, setCurrentImageIndex ] = useState(0);
+  const imagePaths = images;
 
+  useEffect(() => {
+    let interval = null;
+
+    if (isTransitioning) {
+      interval = setInterval(() => {
+        setCurrentImageIndex((prevIndex) => {
+          if (prevIndex === imagePaths.length - 1) {
+            return 0; 
+          }
+          else {
+            return prevIndex + 1; 
+          }
+        });
+      }, 150); 
+    }
+
+    if (currentImageIndex === stopIndex) {
+      setCurrentImageIndex(0); 
+      clearInterval(interval);
+    }
+
+    return () => {
+      clearInterval(interval); 
+    };
+  }, [ isTransitioning ]);
+
+  return (
+    <Image
+      src={imagePaths[currentImageIndex]}
+      width={width}
+      height={height}
+      alt="Picture of the author"
+      style={{
+        position: "absolute", right: right, left: left, bottom: bottom, zIndex: zIndex ,userSelect: "none"
+      }}
+      className={className ? className : ""}
+    />
+  );
+};
 
 
 const TypeFastGame = () => {
@@ -55,46 +110,18 @@ const TypeFastGame = () => {
   const quoteContentRef = useRef("");
   const correctWordsRef = useRef([]);
   const enemyHp = useRef(100);
-
-
+  const [ isTransitioningEnemy, setIsTransitioningEnemy ] = useState(false);
+  const [ isTransitioning, setIsTransitioning ] = useState(false);
+  const myBoxer = [ idle, prepare_jab, jab ];
+  const enemyBoxer = [ Opponent_iddle,Opponent_prepare_jab, Opponent_jab ];
   const [ myHp, setMyHp ] = useState(100);
-
-  const decreaseHp = () => {
-    const threshold = 10;
-
-    const interval = setInterval(() => {
-      setMyHp((prevHp) => {
-        if (prevHp <= 0) {
-          clearInterval(interval);
-          return prevHp;
-        }
-
-        const deduction = Math.min(Math.floor(Math.random() * threshold) + 1, prevHp);
-        const newHp = prevHp - deduction;
-        return newHp;
-      });
-    }, Math.floor(Math.random() * (3500 - 2500 + 1)) + 2500);
-  };
-
+  const [ gameOver, setGameOver ] = useState(false);
+  const [ startGame, setStartGame ] = useState(false);
+  const [ timeoutValue, setTimeoutValue ] = useState(3);
+  const [ lastWordLength, setLastWordLength ] = useState(0);
+  const [ isShaking, setIsShaking ] = useState(false);
 
   
-  const [ gameOver, setGameOver ] = useState(false);
-
-  const resetGame = () => {
-    setActiveWordIndex(0);
-    correctWordsRef.current = [];
-    setQuoteInput("");
-    setMyHp(100);
-    enemyHp.current = 100;
-    setGameOver(false);
-    setStartGame(false);
-    getRandomQuote()
-
-  };
-
-
-  const [ startGame, setStartGame ] = useState(false);
-
   useEffect(() => {
     if (enemyHp.current <= 0 || myHp <= 0) {
       setGameOver(true);
@@ -108,29 +135,124 @@ const TypeFastGame = () => {
   }, [ gameOver ]);
 
 
+  useEffect(() => {
+    let timeout;
+    let interval;
+
+    if (startGame) {
+      timeout = setTimeout(() => {
+        interval = setInterval(() => {
+          setMyHp((prevCount) => {
+            if (prevCount <= 0) {
+              clearInterval(interval);
+              return prevCount;
+            }
+
+            const deduction = Math.floor(Math.random() * 6) + 5;
+            const newCount = prevCount - deduction;
+            return newCount;
+          });
+
+          setIsTransitioningEnemy(true);
+          setTimeout(() => {
+            setIsTransitioningEnemy(false);
+          }
+          , 450);
+
+          setIsShaking(true)
+   
+
+        }, 1000);
+      }, 3000);
+    }
+
+    return () => {
+      clearTimeout(timeout);
+      clearInterval(interval);
+    };
+  }, [ startGame ]);
+
+  useEffect(() => {
+    if (startGame && timeoutValue > 0) {
+      const timer = setInterval(() => {
+        setTimeoutValue((prevValue) => prevValue - 1);
+      }, 1000);
+
+      return () => {
+        clearInterval(timer);
+      };
+    }
+
+    if(!startGame){
+      getRandomQuote()
+    }
+  }, [ startGame, timeoutValue ]);
+
+  useEffect(() => {
+    if (isShaking) {
+      document.querySelector('.wrapper').classList.add('shake-animation');
+
+      setTimeout(() => {
+        setIsShaking(false);
+        document.querySelector('.wrapper').classList.remove('shake-animation');
+      }, 1000);
+    }
+  }, [ isShaking ]);
+
+
+  
+  const resetGame = () => {
+    setActiveWordIndex(0);
+    correctWordsRef.current = [];
+    setQuoteInput("");
+    setMyHp(100);
+    enemyHp.current = 100;
+    setGameOver(false);
+    setStartGame(false);
+    setTimeoutValue(3);
+    setLastWordLength(0);
+
+  };
+
+  console.log("lastWordLength", quoteContentRef.current);
+
   const displayText = () => {
     const text = quoteContentRef.current?.split(" ");
+    console.log("text", text)
     if (!text) {
       return null;
     }
-    return text.map((word, index) => (
-      <Word
-        key={index}
-        word={word}
-        activeWord={index === activeWordIndex}
-        correct={correctWordsRef.current[index]}
-      />
-    ));
-  };
 
+    const firstLength = text[0].length;
+  
+    return (
+      <div className="quote-wrapper">
+        <div className="quote-container" style={{ left: `calc(50% - ${(lastWordLength * 15) + (activeWordIndex* 10) }px  - ${(firstLength * 6)}px)` }}>
+          {text.map((word, index) => { 
+            if ( word === ""){
+              return null
+            }
+            return(
+              <Word
+                key={index}
+                word={word}
+                activeWord={index === activeWordIndex}
+                correct={correctWordsRef.current[index]}
+              />
+            )
+          })}
+        </div>
+      </div>
+    );
+  };
 
   const getRandomQuote = async () => {
     try {
       const response = await fetch(RANDOM_QUOTE_API_URL);
       const data = await response.json();
-      return Promise.resolve(data.content).then((content) => {
-        quoteContentRef.current = content;
-      })
+      const content = data.content.toLowerCase().replace(/[^a-z\s]+/g, '');
+      quoteContentRef.current = content;
+      return Promise.resolve(content);
     }
     catch (error) {
       console.error("Error fetching random quote:", error);
@@ -139,22 +261,19 @@ const TypeFastGame = () => {
   };
 
 
-  useEffect(() => {
-    getRandomQuote()
-
-  }, []);
-
-  
-  console.log("quoteInput", quoteInput);
-
-
   const handleUserInput = (value) => {
     if (value.endsWith(" ")) {
       const lastWord = quoteContentRef.current.split(" ")[activeWordIndex];
-      if (lastWord === value.trim()) {
+      if (lastWord === value.trim() && timeoutValue <= 1 ) {
         setActiveWordIndex((prevIndex) => prevIndex + 1);
         correctWordsRef.current = [ ...correctWordsRef.current, value.trim() ];
-        enemyHp.current -= 10;
+        setLastWordLength(lastWordLength + lastWord.length);
+        enemyHp.current -= lastWord.length;
+        setIsTransitioning(true);
+        setTimeout(() => {
+          setIsTransitioning(false);
+        }
+        , 450);
       }
       setQuoteInput("");
     }
@@ -164,23 +283,41 @@ const TypeFastGame = () => {
     }
   };
 
+ 
+
+  
+
+
 
   return (
     <div className="wrapper">
-      <div className="flex justify-center items-center font-bold bg-yellow-400	 gap-12 h-20 border-2 border-black"> 
+      <div className="flex justify-center items-center font-bold absolute top-0	w-full z-30 	 gap-12 h-16 "> 
         <HealthBar color="red" health={enemyHp.current} />
-
-        <h1 className="text-black text-2xl  ">VS</h1>
-        {/* <h1 className="text-green-500 ">{myHp}</h1> */}
+        <h1 className="text-yellow-500ss text-2xl   ">VS</h1>
         <HealthBar color="green" health={myHp} />
       </div>
+      {/* <Boxer className={startGame && " in-animation bounce-animation "} images={myBoxer} isTransitioning={isTransitioning} right="calc(50% - 240px - 50px)" bottom="10%" zIndex={20} /> */}
+      {/* <Boxer className={startGame && "bounce-animation2"} images={enemyBoxer} isTransitioning={isTransitioningEnemy} right="calc(50% - 350px - 50px)" bottom="8%" height={700} width={700} zIndex={1} /> */}
+
+      <Boxer className={startGame && "bounce-animation transition "} images={myBoxer} zIndex={20} isTransitioning={isTransitioning} right={ `calc((50% - 240px - 50px) * (${timeoutValue}*0.23 + 1)` }
+        bottom={`9%`}
+      />
+      <Boxer className={startGame && "bounce-animation2 transition"} images={enemyBoxer} zIndex={1} isTransitioning={isTransitioningEnemy} right={ `calc((50% - 350px - 50px) * ( (${timeoutValue}* -0.23 + 1)` } bottom="8%" height={700} width={700} />
+
      
 
-
-      <div className="container">
-        <p className="quote">{displayText()}</p>
-        <input
+      {!startGame && <button className="start-button" onClick={() => setStartGame(true)}>Start</button> }
+      {startGame && timeoutValue > 0 && (
+        <div key={timeoutValue} className="countdown animate" >
+          {timeoutValue}
+        </div>
+      )}
       
+
+      <div className="container" >
+        { startGame && displayText()}
+
+        <input
           id="quoteInput"
           type="text"
           className="quote-input"
@@ -189,7 +326,7 @@ const TypeFastGame = () => {
           value={quoteInput}
           onChange={(e) => handleUserInput(e.target.value)}
           style={{ color: "black" }}
-        ></input>{" "}
+        ></input>
       </div>
     </div>
   );
